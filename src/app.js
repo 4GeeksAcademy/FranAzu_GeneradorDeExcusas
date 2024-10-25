c3a254534c5f24b7a73f09479b6e312c44589420
FranAzuara <franazuara@gmail.com>
2024-10-25 18:54:00 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,32 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function RandomElement(list) {
+    let element = "";
+    element = list[Math.floor(Math.random() * list.length)];
+    return element;
+  }
+  RandomElement(who);
+
+  let excuseGenerator = document.querySelector("#excuse");
+  console.log(excuseGenerator);
+  excuseGenerator.innerHTML =
+    RandomElement(who) +
+    " " +
+    RandomElement(action) +
+    " " +
+    RandomElement(what) +
+    " " +
+    RandomElement(when);
 };
